c1af742c15c05fc22f2bdbeaeefed0bc38377cd6
Renato Groffe <renato.groffe@yahoo.com.br>
2017-11-20 19:17:41 -0200

    Entity Framework Core 2.0 + PostgreSQL
    Exemplos de utilização do Entity Framework Core 2.0 e do PostgreSQL em
    aplicações construídas com o .NET Core 2.0.

---
+++ Exemplos_EFCore2/ScriptsPostgreSQL/Cotacoes.sql
@@ -0,0 +1,44 @@
+﻿CREATE TABLE "Cotacoes"(
+	"Sigla" char(3) NOT NULL,
+	"Nome_Moeda" varchar(30) NOT NULL,
+	"Ultima_Cotacao" timestamp NOT NULL,
+	"Valor_Comercial" numeric (18,4) NOT NULL,
+	"Valor_Turismo" numeric (18,4) NULL,
+	CONSTRAINT PK_Cotacoes PRIMARY KEY ("Sigla")
+);
+
+
+INSERT INTO "Cotacoes"
+           ("Sigla"
+           ,"Nome_Moeda"
+           ,"Ultima_Cotacao"
+           ,"Valor_Comercial"
+           ,"Valor_Turismo")
+     VALUES
+           ('USD'
+           ,'Dólar norte-americano'
+           ,'2017-11-06 12:20'
+           ,3.2874
+		   ,3.4200);
+
+INSERT INTO "Cotacoes"
+           ("Sigla"
+           ,"Nome_Moeda"
+           ,"Ultima_Cotacao"
+           ,"Valor_Comercial")
+     VALUES
+           ('EUR'
+           ,'Euro'
+           ,'2017-11-06 12:20'
+           ,3.8081);
+
+INSERT INTO "Cotacoes"
+           ("Sigla"
+           ,"Nome_Moeda"
+           ,"Ultima_Cotacao"
+           ,"Valor_Comercial")
+     VALUES
+           ('LIB'
+           ,'Libra esterlina'
+           ,'2017-11-06 12:20'
+           ,4.3097);
---
+++ Exemplos_EFCore2/ScriptsPostgreSQL/Produtos.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE "Produtos"(
+	"Codigo" char(5) NOT NULL,
+	"Descricao" varchar(50) NOT NULL,
+	"Ativo" boolean NOT NULL DEFAULT true,
+	CONSTRAINT PK_Produtos PRIMARY KEY ("Codigo")
+);
+
+
+INSERT INTO "Produtos"("Codigo", "Descricao")
+VALUES ('00001', 'Cadeira');
+
+INSERT INTO "Produtos"("Codigo", "Descricao")
+VALUES ('00002', 'Mesa');
+
+INSERT INTO "Produtos"("Codigo", "Descricao")
+VALUES ('00003', 'Sofá');
+
+INSERT INTO "Produtos"("Codigo", "Descricao", "Ativo")
+VALUES ('00004', 'Cadeira de Balanço', false);
